bcae712bda3d4e4062236478be7c6fc06d4ada71
Lunna SoSa <lunna.sosa@uptc.edu.co>
2024-11-04 12:13:13 -0500

    Add components LogOut and Search

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,13 @@
 
 import '../global.css'
-
+import ButtonLogOut from '@/components/ButtonLogOut'
+import Search from '@/components/Search'
 const Home = () => {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-red-600">
+        <div className="flex items-center justify-center min-h-screen bg-gray-700">
             <h1 className="text-4xl font-bold text-blue-600">Welcome to Home Page</h1>
+            {/* <ButtonLogOut/> */}
+            <Search/>
         </div>
     );
 };
